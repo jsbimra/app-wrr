32f76350ab8d60050ae0f2a03a93171f2ff9e838
jsbimra <jsbimra@users.noreply.github.com>
2018-12-22 14:16:54 +0530

    Updates

---
+++ precache-manifest.58ad65d9b0fd8dba02d863b84a3ae735.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eaa2b5c60586749ad2d",
+    "url": "/app-wrr/static/js/runtime~main.2eaa2b5c.js"
+  },
+  {
+    "revision": "0396f44d649fc726a884",
+    "url": "/app-wrr/static/js/main.0396f44d.chunk.js"
+  },
+  {
+    "revision": "4226d1ab32738adbb343",
+    "url": "/app-wrr/static/js/1.4226d1ab.chunk.js"
+  },
+  {
+    "revision": "364db0f8eb08585fa3a418d7c1e97d77",
+    "url": "/app-wrr/index.html"
+  }
+];
